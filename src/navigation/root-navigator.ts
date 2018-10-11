bc4345da6bdd5dd205daca7b71130c17b351ce7d
Amy Huyen <awh4@rice.edu>
2018-10-10 20:50:47 -0500

    Tested react navigation

--- src/navigation/root-navigator.ts
+++ src/navigation/root-navigator.ts
@@ -1,9 +1,14 @@
 import { createStackNavigator } from "react-navigation"
 import { ExampleNavigator } from "./example-navigator"
+import { ExampleTabNavigator } from "./example-tab-navigator"
 
 export const RootNavigator = createStackNavigator(
   {
+    // this is an example of having multiple navigators being rendered by a single navigator
+    // there are multiple types here: stack and tab navigators; navigate to them just as you would navigate
+    // to any other screen 
     exampleStack: { screen: ExampleNavigator },
+    exampleTab: {screen: ExampleTabNavigator},
   },
   {
     headerMode: "none",
