8b7ef325de9fd958f80cced89e81efa8692b832e
Jamie <jlt10@rice.edu>
2018-11-10 14:23:08 -0600

    wrote action that gets all orders for user with input netid

--- src/app/stores/order-store.ts
+++ src/app/stores/order-store.ts
@@ -1,5 +1,33 @@
 import { types, destroy } from "mobx-state-tree";
 import { Location } from "./vendorStore";
+import { client } from "../main";
+import gql from "graphql-tag";
+
+const GET_ORDERS = gql`
+    {
+        query user($netid: String!) {
+            user(netid: $netid) {
+                orders{
+                    location{
+                        name
+                    }
+                    items{
+                        item{
+                            id
+                        }
+                        quantity
+                    }
+                    vendor{
+                        id
+                    }
+                    user{
+                        netid
+                    }
+                }
+            }
+        }  
+    }
+`
 
 const OrderItem = types
 .model('OrderItem' , {
@@ -19,5 +47,17 @@ export const OrderStoreModel = types
 .model('OrderStoreModel', {
     orders : types.array(Order)
 })
+.actions(
+    (self) => ({
+        async getOrders(netid) {
+            let orders = await client.query({
+                query: GET_ORDERS,
+                variables: {netid: netid}
+            });
+            console.log(orders);
+            return orders;
+        }
+    })
+)
 
  export type OrderStore = typeof OrderStoreModel.Type
