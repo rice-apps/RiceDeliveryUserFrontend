f39bfdf84e38db88b65f359edf9df61b2e45063d
Jonathan Cai <caimjonathan@gmail.com>
2018-11-11 12:12:58 -0600

    Refactor, added some documentation to menu screen...

--- src/views/example/menu-screen/menu-item.tsx
+++ src/views/example/menu-screen/menu-item.tsx
@@ -1,30 +1,30 @@
 import * as React from "react"
-import { MenuItem as MenuItemModel } from '../../../app/stores/vendorStore'
+import { MenuItem as MenuItemModel } from "../../../app/stores/vendorStore"
 import { View, Text, TextStyle, TouchableHighlight } from "react-native"
-import { CartStore } from "../../../app/stores/cart-store";
+import { CartStore } from "../../../app/stores/cart-store"
 
 const BOLD: TextStyle = { fontWeight: "bold" }
-
 const UNSELECTED_STYLE: TextStyle = {
     ...BOLD,
-    fontSize: 30,
+    fontSize: 25,
     lineHeight: 30,
     textAlign: "center",
     letterSpacing: 1.5,
     color: "white",
 }
-
 const SELECTED_STYLE: TextStyle = {
     ...UNSELECTED_STYLE,
     backgroundColor: "grey",
 }
 
+// Letting each menuItem have access to entire cart store
 export interface MenuItemProps {
     menuItem?: MenuItemModel;
     cartStore?: CartStore;
 }
 
 export class MenuItem extends React.Component<MenuItemProps, { selected: Boolean }> {
+    // Initialize state to reflect the cart store
     constructor(props) {
         super(props);
         let { cartItems } = this.props.cartStore;
@@ -32,6 +32,7 @@ export class MenuItem extends React.Component<MenuItemProps, { selected: Boolean
         this.state = {selected : isInCart == -1 ? false : true };
     }
 
+    // Toggle select to remove/add to cart
     _onPressButton() {
         let { addToCart, removeFromCart } = this.props.cartStore;
         let { menuItem } = this.props;
@@ -42,11 +43,12 @@ export class MenuItem extends React.Component<MenuItemProps, { selected: Boolean
 
     render() {
         let name = this.props.menuItem.name;
+        // Currently, only displaying first element
         let price = this.props.menuItem.prices[0].price;
         let { selected } = this.state;
-
         return (
             <View>
+                {/* Must bind the button to this component */}
                 <TouchableHighlight onPress = {this._onPressButton.bind(this)}>
                 <Text style = {selected ? SELECTED_STYLE : UNSELECTED_STYLE}>{ name } { price } </Text>
                 </TouchableHighlight>
--- src/views/example/menu-screen/menu-screen.tsx
+++ src/views/example/menu-screen/menu-screen.tsx
@@ -3,16 +3,11 @@ import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView, SectionLis
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
 import { Text } from "../../shared/text"
-import { Button } from "../../shared/button"
 import { Wallpaper } from "../../shared/wallpaper"
 import { Header } from "../../shared/header"
 import { color, spacing } from "../../../theme"
-import { logoIgnite, heart } from "./"
-import { BulletItem } from "../bullet-item"
-import { Api } from "../../../services/api"
-import { save } from "../../../lib/storage"
 import { inject, observer } from "mobx-react"
-import { RootStore } from "../../../app/root-store";
+import { RootStore } from "../../../app/root-store"
 import { MenuItem } from "./menu-item"
 
 const FULL: ViewStyle = { flex: 1 }
@@ -20,17 +15,7 @@ const CONTAINER: ViewStyle = {
   backgroundColor: color.transparent,
   paddingHorizontal: spacing[4],
 }
-const DEMO: ViewStyle = {
-  paddingVertical: spacing[4],
-  paddingHorizontal: spacing[4],
-  backgroundColor: "#5D2555",
-}
 const BOLD: TextStyle = { fontWeight: "bold" }
-const DEMO_TEXT: TextStyle = {
-  ...BOLD,
-  fontSize: 13,
-  letterSpacing: 2,
-}
 const HEADER: TextStyle = {
   paddingTop: spacing[3],
   paddingBottom: spacing[5] - 1,
@@ -50,32 +35,6 @@ const TITLE: TextStyle = {
   textAlign: "center",
   marginBottom: spacing[5],
 }
-const TAGLINE: TextStyle = {
-  color: "#BAB6C8",
-  fontSize: 15,
-  lineHeight: 22,
-  marginBottom: spacing[4] + spacing[1],
-}
-const IGNITE: ImageStyle = {
-  marginVertical: spacing[6],
-  alignSelf: "center",
-}
-const LOVE_WRAPPER: ViewStyle = {
-  flexDirection: "row",
-  alignItems: "center",
-  alignSelf: "center",
-}
-const LOVE: TextStyle = {
-  color: "#BAB6C8",
-  fontSize: 15,
-  lineHeight: 22,
-}
-const HEART: ImageStyle = {
-  marginHorizontal: spacing[2],
-  width: 10,
-  height: 10,
-  resizeMode: "contain",
-}
 
 export interface MenuScreenProps extends NavigationScreenProps<{}> {
   rootStore?: RootStore
@@ -88,6 +47,7 @@ export interface MenuScreenProps extends NavigationScreenProps<{}> {
 @inject("rootStore")
 @observer
 export class MenuScreen extends React.Component<MenuScreenProps, { vendorName: String, vendorMenu: Array<Object> }> {
+  // Loading in first vendor from vendorStore
   constructor(props) {
     super(props);
     let { vendor: vendorArray } = this.props.rootStore.vendorStore;
@@ -98,41 +58,6 @@ export class MenuScreen extends React.Component<MenuScreenProps, { vendorName: S
   }
 
   goBack = () => this.props.navigation.goBack(null)
-
-  demoReactotron = async () => {
-    console.tron.log("Your Friendly tron log message")
-    console.tron.logImportant("I am important")
-    console.tron.display({
-      name: "DISPLAY",
-      value: {
-        numbers: 1,
-        strings: "strings",
-        booleans: true,
-        arrays: [1, 2, 3],
-        objects: {
-          deeper: {
-            deeper: {
-              yay: "👾",
-            },
-          },
-        },
-        functionNames: function hello() { },
-      },
-      preview: "More control with display()",
-      important: true,
-      image: {
-        uri:
-          "https://avatars2.githubusercontent.com/u/3902527?s=200&u=a0d16b13ed719f35d95ca0f4440f5d07c32c349a&v=4",
-      },
-    })
-    // make an API call for the demo
-    // Don't do API like this, use store's API
-    const demo = new Api()
-    demo.setup()
-    demo.getUser("1")
-    // Let's do some async storage stuff
-    await save("Cool Name", "Boaty McBoatface")
-  }
 
   render() {
     let { cartStore } = this.props.rootStore;
@@ -150,21 +75,22 @@ export class MenuScreen extends React.Component<MenuScreenProps, { vendorName: S
               titleStyle={HEADER_TITLE}
             />
 
+            {/* Section List to render in each item on menu */}
             <SectionList
+              // Passing in the entire cart store into each menuItem
               renderItem={({ item, index, section }) => <MenuItem menuItem = { item } cartStore = { cartStore }></MenuItem>}
               renderSectionHeader={({ section: { title } }) => (
                 <Text style={TITLE}>{title}</Text>
               )}
               sections={[
-                { title: vendorName + '\'s Menu', data: vendorMenu },
-                // { title: 'Title2', data: ['item3', 'item4'] },
-                // { title: 'Title3', data: ['item5', 'item6'] },
+                { title: vendorName + "\'s Menu", data: vendorMenu },
               ]}
               keyExtractor={(item, index) => item + index}
             />
 
+              {/* Access cart store just to debug */}
             <TouchableHighlight onPress = {() => console.log(this.props.rootStore.cartStore)}>
-              <Text>Get Cart Store</Text>
+              <Text>Checkout Cart Store</Text>
             </TouchableHighlight>
 
           </Screen>
