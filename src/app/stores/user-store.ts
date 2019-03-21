a3e91406dd1ef04ea249dea39ab4f1fa8adb69cd
Amy Huyen <awh4@rice.edu>
2019-03-20 21:22:23 -0500

    Fixed user store so that registration page shows up

--- src/app/stores/user-store.ts
+++ src/app/stores/user-store.ts
@@ -32,8 +32,8 @@ export const UserStoreModel = types
     hasAccount: types.optional(types.boolean, false)
 })
 .actions(
-    (self) => {
-        async function authenticate(ticket) {
+    (self) => ({
+        async authenticate(ticket) {
             console.log("HERE");
             let user = await client.mutate({
                 mutation: AUTHENTICATION,
@@ -44,13 +44,13 @@ export const UserStoreModel = types
             console.log(user.data.authenticator);
 
             console.log("Almost authenticated");
-            setAuth(true);
+            self.setAuth(true);
             if (user.data.authenticator.firstName != null) {
-                setUser(user.data.authenticator);
-                setAccountState(true);
+                self.setUser(user.data.authenticator);
+                self.setAccountState(true);
             } else {
-                setUser({ netID: user.data.authenticator.netID });
-                setAccountState(false);
+                self.setUser({ netID: user.data.authenticator.netID });
+                self.setAccountState(false);
             }
 
             // api
@@ -75,31 +75,24 @@ export const UserStoreModel = types
             // console.log(user);
             // console.log(res);
             // });
-        }
-        async function setUser(new_user) {
+        },
+        setUser(new_user) {
             console.log("THIS IS THE NEW USER OBJECT: " + JSON.stringify(new_user));
             console.log("THIS IS THE OLD USER OBJECT: " + JSON.stringify(self.user));
             self.user = new_user;
-            
             console.log("THIS IS THE UPDATED USER OBJECT: " + JSON.stringify(self.user));
 
-        }
-        function setAccountState(accountState) {
+        },
+        setAccountState(accountState) {
             self.hasAccount = accountState;
-        }
+        },
         // loggedIn() {
         //     return self.authenticated ? true : false
         // },
-        function setAuth(authState) {
+        setAuth(authState) {
             self.authenticated = authState;
-        }
-        return {
-            authenticate,
-            setUser,
-            setAccountState,
-            setAuth
-        }
-    }
+        },
+    })
 )
 
 // .create({
