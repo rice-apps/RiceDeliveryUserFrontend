2a75c671891c197a941ded3db92a0be574e1867c
Jonathan Cai <caimjonathan@gmail.com>
2019-04-10 00:48:28 -0500

    removed build-breaking bug w duplicate locationPickerItems

--- src/app/screens/menuStack/checkout-screen/checkout-screen.tsx
+++ src/app/screens/menuStack/checkout-screen/checkout-screen.tsx
@@ -193,15 +193,8 @@ export class CheckoutScreen extends React.Component<CheckoutScreenProps, any> {
       return <Picker.Item key={i} value={s.name} label={s.name} />
     });
 
-
-
   console.log("locationOptions");
   console.log(locationOptions);
-
-  let locationPickerItems = locationOptions.map((s, i) => {
-    return <Picker.Item key={i} value={s.name} label={s.name} />
-  });
-
 
     return (
       <View style={css.screen.defaultScreen}>
